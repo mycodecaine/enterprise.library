3fb804e1e2b265c6be786dd17729311be50d6053
Codecaine <heemi@codecaine.my>
2024-11-01 11:31:50 +0800

    added unit test

---
+++ test/Cdcn.Enterprise.Library.Tests/Domain/Primitives/ValueObjectTests.cs
@@ -0,0 +1,92 @@
+﻿using Cdcn.Enterprise.Library.Domain.Primitives;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cdcn.Enterprise.Library.Tests.Domain.Primitives
+{
+    [TestFixture]
+    public class ValueObjectTests
+    {
+        [Test]
+        public void EqualValueObjectsShouldBeEqual()
+        {
+            var valueObject1 = new SampleValueObject(1, "Test");
+            var valueObject2 = new SampleValueObject(1, "Test");
+
+            Assert.IsTrue(valueObject1 == valueObject2);
+            Assert.IsFalse(valueObject1 != valueObject2);
+            Assert.AreEqual(valueObject1, valueObject2);
+        }
+
+        [Test]
+        public void DifferentValueObjectsShouldNotBeEqual()
+        {
+            var valueObject1 = new SampleValueObject(1, "Test");
+            var valueObject2 = new SampleValueObject(2, "Test");
+
+            Assert.IsFalse(valueObject1 == valueObject2);
+            Assert.IsTrue(valueObject1 != valueObject2);
+            Assert.AreNotEqual(valueObject1, valueObject2);
+        }
+
+        [Test]
+        public void NullValueObjectShouldNotEqualNonNull()
+        {
+            var valueObject = new SampleValueObject(1, "Test");
+            SampleValueObject? nullValueObject = null;
+           
+            Assert.IsFalse(valueObject == nullValueObject);
+            Assert.IsNull(nullValueObject);
+        }
+
+        [Test]
+        public void SameInstanceShouldBeEqual()
+        {
+            var valueObject = new SampleValueObject(1, "Test");
+
+            Assert.AreEqual(valueObject, valueObject);
+        }
+
+        [Test]
+        public void GetHashCodeShouldBeConsistent()
+        {
+            var valueObject1 = new SampleValueObject(1, "Test");
+            var valueObject2 = new SampleValueObject(1, "Test");
+
+            Assert.AreEqual(valueObject1.GetHashCode(), valueObject2.GetHashCode());
+        }
+
+        [Test]
+        public void DifferentValuesShouldHaveDifferentHashCodes()
+        {
+            var valueObject1 = new SampleValueObject(1, "Test");
+            var valueObject2 = new SampleValueObject(2, "Test");
+
+            Assert.AreNotEqual(valueObject1.GetHashCode(), valueObject2.GetHashCode());
+        }
+    }
+
+    public class SampleValueObject : ValueObject
+    {
+        public int Id { get; }
+        public string Name { get; }
+
+        public SampleValueObject(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        protected override IEnumerable<object> GetAtomicValues()
+        {
+            yield return Id;
+            yield return Name;
+        }
+    }
+
+
+}
