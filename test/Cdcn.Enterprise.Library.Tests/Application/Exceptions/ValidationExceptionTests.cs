e72f1c3dca872b7c541c5fd6268454e243fcc1ea
Codecaine <heemi@codecaine.my>
2024-11-01 10:06:54 +0800

    added unit test

---
+++ test/Cdcn.Enterprise.Library.Tests/Application/Exceptions/ValidationExceptionTests.cs
@@ -0,0 +1,35 @@
+﻿using Cdcn.Enterprise.Library.Application.Exceptions;
+using FluentValidation.Results;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cdcn.Enterprise.Library.Tests.Exceptions
+{
+    internal class ValidationExceptionTests
+    {
+        [Test]
+        public void Constructor_WithValidationFailures_ShouldSetErrorsProperty()
+        {
+            // Arrange
+            var failures = new List<ValidationFailure>
+        {
+            new ValidationFailure { ErrorCode = "Code1", ErrorMessage = "Error message 1" },
+            new ValidationFailure { ErrorCode = "Code2", ErrorMessage = "Error message 2" },
+            
+        };
+
+            // Act
+            var exception = new ValidationException(failures);
+
+            // Assert
+            Assert.AreEqual("One or more validation failures has occurred.", exception.Message);
+            Assert.NotNull(exception.Errors);
+            Assert.AreEqual(2, exception.Errors.Count); // Distinct by ErrorCode and ErrorMessage
+            Assert.IsTrue(exception.Errors.Any(e => e.Code == "Code1" && e.Message == "Error message 1"));
+            Assert.IsTrue(exception.Errors.Any(e => e.Code == "Code2" && e.Message == "Error message 2"));
+        }
+    }
+}
---
+++ test/Cdcn.Enterprise.Library.Tests/Domain/Exceptions/DomainExceptionTests.cs
@@ -0,0 +1,39 @@
+﻿using Cdcn.Enterprise.Library.Domain.Exceptions;
+using Cdcn.Enterprise.Library.Domain.Primitives;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cdcn.Enterprise.Library.Tests.Exceptions
+{
+    internal class DomainExceptionTests
+    {
+        [Test]
+        public void Constructor_ShouldSetErrorProperty()
+        {
+            // Arrange
+            var error = new Error("Code1", "An error occurred.");
+
+            // Act
+            var exception = new DomainException(error);
+
+            // Assert
+            Assert.AreEqual(error, exception.Error);
+        }
+
+        [Test]
+        public void Constructor_ShouldSetMessageToErrorMessage()
+        {
+            // Arrange
+            var error = new Error("Code1", "An error occurred.");
+
+            // Act
+            var exception = new DomainException(error);
+
+            // Assert
+            Assert.AreEqual("An error occurred.", exception.Message);
+        }
+    }
+}
