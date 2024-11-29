67a42f0d61ac10f7f97e1b72425587dc8e1e03ab
heemi <heemi@softwareq.com>
2024-11-29 10:46:10 +0800

    update package

--- src/Cdcn.Enterprise.Library.Application/Mediator/Base/BaseHandler.cs
+++ src/Cdcn.Enterprise.Library.Application/Mediator/Base/BaseHandler.cs
@@ -5,11 +5,17 @@ using Microsoft.Extensions.Logging;
 
 namespace Cdcn.Enterprise.Library.Application.Mediator.Base
 {
-    public abstract class BaseHandler<TRequest, TResult>
+    public class BaseHandler<TRequest, TResult>
     {
+        private readonly ILogger _logger;
+
+        public BaseHandler(ILogger logger)
+        {
+            _logger = logger;
+        }
+
         protected async Task<TResult> HandleSafelyAsync(
-            Func<Task<TResult>> handleCoreAsync,
-            ILogger logger,string context="")
+            Func<Task<TResult>> handleCoreAsync, string context = "")
         {
             try
             {
@@ -17,18 +23,18 @@ namespace Cdcn.Enterprise.Library.Application.Mediator.Base
             }
             catch (ServiceInfrastructureException ex)
             {
-                logger?.LogError(ex, "An ServiceInfrastructureException occurred.");
+                _logger?.LogError(ex, "An ServiceInfrastructureException occurred.");
                 throw; // Re throw the original exception.
             }
             catch (EnterpriseLibraryException ex)
             {
-                logger?.LogError(ex, "An EnterpriseLibraryException occurred.");
+                _logger?.LogError(ex, "An EnterpriseLibraryException occurred.");
                 throw; // Re throw the original exception.
             }
             catch (Exception ex)
-            {                
-                logger?.LogError(ex, "An unexpected error occurred.");
-                throw HandleException.ThrowServiceApplicationException(ex, logger, $"{context ?? this.GetType().FullName}");
+            {
+                _logger?.LogError(ex, "An unexpected error occurred.");
+                throw HandleException.ThrowServiceApplicationException(ex, _logger, $"{context ?? this.GetType().FullName}");
             }
         }
     }
