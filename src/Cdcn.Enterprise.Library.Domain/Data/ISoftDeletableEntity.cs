18fb3ae4adfe5fe544e46210eab3c254f89679e3
heemi <heemi@softwareq.com>
2025-01-30 23:55:55 +0800

    softdelete

--- src/Cdcn.Enterprise.Library.Domain/Data/ISoftDeletableEntity.cs
+++ src/Cdcn.Enterprise.Library.Domain/Data/ISoftDeletableEntity.cs
@@ -2,12 +2,12 @@
 {
     /// <summary>
     /// Interface to enable entity to be soft deleting.
+    /// Notes : https://chatgpt.com/share/6799694a-3bfc-8007-b1f0-dff7fa32aba4
     /// </summary>
     public interface ISoftDeletableEntity
     {
         /// <summary>
-        /// Gets the date and time in UTC format the entity was deleted on.
-        /// Notes : https://chatgpt.com/share/6799694a-3bfc-8007-b1f0-dff7fa32aba4
+        /// Gets the date and time in UTC format the entity was deleted on.        
         /// </summary>
         DateTime? DeletedOnUtc { get; }
 
