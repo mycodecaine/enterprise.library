eab3a0ccbf6a23df482272fd8120ddd8314b04df
heemi <heemi@softwareq.com>
2025-02-05 06:03:26 +0800

    refactor HttpService

--- src/Cdcn.Enterprise.Library.Infrastructure/Services/HttpService.cs
+++ src/Cdcn.Enterprise.Library.Infrastructure/Services/HttpService.cs
@@ -8,53 +8,53 @@ namespace Cdcn.Enterprise.Library.Infrastructure.Services
 {
     public class HttpService : IHttpService
     {
-        
         private readonly IHttpClientFactory _httpClientFactory;
-        private ILogger<HttpService> _logger;
+        private readonly ILogger<HttpService> _logger;
 
-        public HttpService( IHttpClientFactory httpClientFactory, ILogger<HttpService> logger)
-        {           
+        public HttpService(IHttpClientFactory httpClientFactory, ILogger<HttpService> logger)
+        {
             _httpClientFactory = httpClientFactory;
             _logger = logger;
         }
 
         public async Task<HttpResponseMessage> PostAsync(string url, List<KeyValuePair<string, string>> content, string authToken = "")
         {
-            var client = _httpClientFactory.CreateClientWithPolicy();
+            var client = CreateClient(authToken);
             var requestContent = new FormUrlEncodedContent(content);
-            if (!string.IsNullOrEmpty(authToken))
-                client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", authToken);
-            // Send a POST request to the token endpoint with the prepared request content
-            var response = await client.PostAsync(url, requestContent);
-            return response;
+            return await client.PostAsync(url, requestContent);
         }
 
         public async Task<HttpResponseMessage> PostJsonAsync<T>(string url, T content, string authToken = "")
         {
-            var client = _httpClientFactory.CreateClientWithPolicy();
-            if (!string.IsNullOrEmpty(authToken))
-                client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", authToken);
-            var response = await client.PostAsJsonAsync(url, content);
-            return response;
+            var client = CreateClient(authToken);
+            return await client.PostAsJsonAsync(url, content);
         }
+
         public async Task<HttpResponseMessage> GetAsync(string url, string authToken = "")
         {
-            var client = _httpClientFactory.CreateClientWithPolicy();
-            if (!string.IsNullOrEmpty(authToken))
-                client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", authToken);
-
-            // Send a POST request to the token endpoint with the prepared request content
-            var response = await client.GetAsync(url);
-            return response;
+            var client = CreateClient(authToken);
+            return await client.GetAsync(url);
         }
 
         public async Task<HttpResponseMessage> PutJsonAsync<T>(string url, T content, string authToken = "")
         {
+            var client = CreateClient(authToken);
+            return await client.PutAsJsonAsync(url, content);
+        }
+
+        /// <summary>
+        /// Creates an HttpClient with the specified authorization token.
+        /// </summary>
+        /// <param name="authToken"></param>
+        /// <returns></returns>
+        private HttpClient CreateClient(string authToken)
+        {
             var client = _httpClientFactory.CreateClientWithPolicy();
             if (!string.IsNullOrEmpty(authToken))
+            {
                 client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", authToken);
-            var response = await client.PutAsJsonAsync(url, content);
-            return response;
+            }
+            return client;
         }
     }
 }
